e0f8f53b6927b3ffd04a81ba67f03484314ebd29
Girmay <ink423103@yahoo.com>
2020-02-14 23:08:10 +0100

    Head component added

---
+++ src/components/Head.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const List = props => {
+  return (    
+         <div>       
+              <h1>
+                  Github Users Search
+              </h1> 
+          </div>   
+          )
+       
+};
+
+
+export default List;
