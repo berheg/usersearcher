bad5c96e839210c82d6819ec48cca4a7efec0bb5
Girmay <ink423103@yahoo.com>
2020-02-15 23:30:32 +0100

    About component added

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is github users search app. It is part of a React course in HackYourFuture</p>
+      <p>You can search github users by thier user-name. Based on your search input aviliable user <br/>accounts are listed where you can click and see detail information of the github user account.</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
